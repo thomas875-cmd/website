65200599df9054594eb1e20f64003cd563b39cc0
Mohamed Abduljawad <salintofficial@hotmail.com>
2021-05-17 14:43:39 +0200

    Fetch profile picture

--- public/static/scripts/Navbar.js
+++ public/static/scripts/Navbar.js
@@ -2,11 +2,20 @@ document.getElementById("nav-button").addEventListener("click", () => {
 	document.querySelector("nav ul").classList.toggle("opened");
 });
 
-firebase.auth().onAuthStateChanged(user => {
+firebase.auth().onAuthStateChanged( async user => {
 	const element = document.querySelector("nav div#profile");
+	const userService = new UserService();
 
 	if(user) {
-		element.innerHTML = `<a href="profile.html"><img src="./static/images/placeholder.jpg" alt="Profile picture" /></a>`;
+		let src;
+		try {
+			const URL = await userService.getProfilePicture(user.uid);
+	
+			src = URL;
+		}
+		finally {
+			element.innerHTML = `<a href="profile.html"><img src="${src}" alt="Profile picture" /></a>`;
+		}
 	}
 	else {
 		element.innerHTML = `<a class="login" href="login.html">تسجيل الدخول</a>`;
